362c75c9d1371d6a7a3d7f0ddf05554029b59f13
Mahesh Waghmare <maheshw@bsf.io>
2020-01-03 19:49:51 +0530

    Updates

---
+++ precache-manifest.c5a26335a81f56cf1fbb1b477b381e8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf9b4c2d9173a729ba2d683608741578",
+    "url": "/reactjs-wp-org-dashboard/index.html"
+  },
+  {
+    "revision": "75553d4e3ee81e99d7e6",
+    "url": "/reactjs-wp-org-dashboard/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "4126018296181b3bb09c",
+    "url": "/reactjs-wp-org-dashboard/static/css/main.b7baf18c.chunk.css"
+  },
+  {
+    "revision": "75553d4e3ee81e99d7e6",
+    "url": "/reactjs-wp-org-dashboard/static/js/2.6e789318.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/reactjs-wp-org-dashboard/static/js/2.6e789318.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4126018296181b3bb09c",
+    "url": "/reactjs-wp-org-dashboard/static/js/main.efebceeb.chunk.js"
+  },
+  {
+    "revision": "dc144d344e33e7b68591",
+    "url": "/reactjs-wp-org-dashboard/static/js/runtime-main.beff522b.js"
+  }
+]);
